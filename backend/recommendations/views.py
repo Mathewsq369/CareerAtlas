d4b6e4e706a3802e7fa4b207602041efa9323943
Mathewsq369 <mathewsq369@gmaail.com>
2025-11-15 19:05:09 +0300

    recommendations/views.py

--- backend/recommendations/views.py
+++ backend/recommendations/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import StudentRecommendation, Career, Subject
+from .serializers import (
+    CareerSerializer, StudentRecommendationSerializer, 
+    SubjectSerializer
+)
+from .services import RecommendationEngine, SubjectRecommender
 
-# Create your views here.
+class CareerViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CareerSerializer
+    queryset = Career.objects.all()
+
+class StudentRecommendationViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = StudentRecommendationSerializer
+    
+    def get_queryset(self):
+        return StudentRecommendation.objects.filter(
+            student__user=self.request.user
+        ).select_related('career').prefetch_related('recommended_subjects')
+    
+    @action(detail=False, methods=['get'])
+    def generate_recommendations(self, request):
+        """Generate new career recommendations"""
+        student_profile = request.user.studentprofile
+        
+        engine = RecommendationEngine(student_profile)
+        recommendations = engine.generate_career_recommendations()
+        
+        # Get the saved recommendations
+        saved_recommendations = self.get_queryset()
+        serializer = self.get_serializer(saved_recommendations, many=True)
+        
+        return Response(serializer.data)
+
+class SubjectRecommendationView(viewsets.ViewSet):
+    @action(detail=False, methods=['get'])
+    def get_recommendations(self, request):
+        """Get subject recommendations"""
+        student_profile = request.user.studentprofile
+        
+        recommender = SubjectRecommender(student_profile)
+        recommended_subjects = recommender.recommend_subjects()
+        
+        # Get full subject objects
+        subjects = Subject.objects.filter(name__in=recommended_subjects)
+        serializer = SubjectSerializer(subjects, many=True)
+        
+        return Response(serializer.data)
