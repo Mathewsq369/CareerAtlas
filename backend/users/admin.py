6f51c9516ca0c664a626de91cd43f9919da95585
Mathewsq369 <mathewsq369@gmaail.com>
2025-11-15 22:12:32 +0300

    users/admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, StudentProfile, School
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'user_type', 'date_joined']
+    list_filter = ['user_type', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('user_type', 'phone_number', 'date_of_birth')}),
+    )
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'school', 'grade_level']
+    list_filter = ['grade_level', 'school']
+    search_fields = ['user__username', 'user__email', 'school__name']
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'county', 'type']
+    list_filter = ['type', 'county']
+    search_fields = ['name', 'code']
