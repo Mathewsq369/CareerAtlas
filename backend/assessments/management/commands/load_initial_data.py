16860755f0df62099398da312732dce750333fb0
Mathewsq369 <mathewsq369@gmaail.com>
2025-11-20 01:25:45 +0300

    edited load_initial_data.py

--- backend/assessments/management/commands/load_initial_data.py
+++ backend/assessments/management/commands/load_initial_data.py
@@ -2,14 +2,14 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
 class Command(BaseCommand):
-    help = 'Load all initial data for EduPersona'
+    help = 'Load all initial data for CareerAtlas'
     
     def handle(self, *args, **options):
-        self.stdout.write('Loading initial data for EduPersona Kenya...')
+        self.stdout.write('Loading initial data for CareerAtlas Kenya...')
         
-        # Load data in correct order to avoid foreign key issues
+        # Load data in correct order to maintain foreign key relationships
         call_command('populate_mbti_data')
-        call_command('populate_learning_styles') 
+        call_command('populate_learning_styles')
         call_command('populate_kenyan_careers')
         call_command('populate_sample_questions')
         
